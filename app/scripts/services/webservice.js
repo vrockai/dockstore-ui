4fd23e5fe89dd310fc6641e653433293cf413075
garyluu <garylau.work@gmail.com>
2016-12-22 15:48:05 -0500

    Reverting webservice.js to what it's supposed to be

--- app/scripts/services/webservice.js
+++ app/scripts/services/webservice.js
@@ -15,21 +15,21 @@
 
 angular.module('dockstore.ui')
   .constant('WebService', {
-    API_URI: 'http://10.0.29.120:8080',
-    API_URI_DEBUG: 'http://10.0.29.120:8080/tests/dummy-data',
+    API_URI: 'http://localhost:8080',
+    API_URI_DEBUG: 'http://localhost:9000/tests/dummy-data',
 
     GITHUB_AUTH_URL: 'https://github.com/login/oauth/authorize',
-    GITHUB_CLIENT_ID: 'fc672e5080a4bfa774c2',
-    GITHUB_REDIRECT_URI: 'http://10.0.29.120:9000/login',
+    GITHUB_CLIENT_ID: 'a70739297a7d67f915de',
+    GITHUB_REDIRECT_URI: 'http://localhost:9000/login',
     GITHUB_SCOPE: 'read:org',
 
     QUAYIO_AUTH_URL: 'https://quay.io/oauth/authorize',
-    QUAYIO_CLIENT_ID: 'QL8RTN8D1309J9VC7TJM',
-    QUAYIO_REDIRECT_URI: 'http://10.0.29.120:9000/auth/quay.io',
+    QUAYIO_CLIENT_ID: 'RWCBI3Y6QUNXDPYKNLMC',
+    QUAYIO_REDIRECT_URI: 'http://localhost:9000/auth/quay.io',
     QUAYIO_SCOPE: 'repo:read,user:read',
 
     BITBUCKET_AUTH_URL: 'https://bitbucket.org/site/oauth2/authorize',
-    BITBUCKET_CLIENT_ID: 'uzE3UWkkfdtvHKPAww',
+    BITBUCKET_CLIENT_ID: 'K9VxydpaXKCgxxVpbx',
 
     DSCLI_RELEASE_URL: 'https://github.com/ga4gh/dockstore/' +
                         'releases/download/0.3-alpha.0/dockstore'
